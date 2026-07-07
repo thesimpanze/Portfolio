34d73bc5329a389e05da6a316590e7130ba2e691
Rasyid Nuruddin <rasyid8nuruddin@gmail.com>
2026-07-07 13:54:05 +0700

    Update Memories.tsx

--- src/sections/Memories.tsx
+++ src/sections/Memories.tsx
@@ -6,7 +6,7 @@ const Memories = () => {
     <div className="w-full h-full">
         <h1 className="text-center text-4xl mb-5">Our Memories.</h1>
       <div className="w-[100vw] h-screen py-3">
-        <DomeGallery />
+       
       </div>
     </div>
   );
